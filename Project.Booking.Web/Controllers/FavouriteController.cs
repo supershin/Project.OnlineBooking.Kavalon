3fab5531965a0bcab3084cbfbc13b549bd3bc99f
Siripoj Sermkiatsakun <supershin2529@gmail.com>
2024-07-25 13:51:39 +0700

    add favourite menu

---
+++ Project.Booking.Web/Controllers/FavouriteController.cs
@@ -0,0 +1,24 @@
+﻿using Project.Booking.Constants;
+using Project.Booking.Extensions;
+using Project.Booking.Model;
+using Project.Booking.Security;
+using Project.Booking.Web.Filters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.Booking.Web.Controllers
+{
+    [CustomAuthenticationFilter]
+    public class FavouriteController : BaseController
+    {
+        // GET: User
+        [Route("favourite/index")]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
